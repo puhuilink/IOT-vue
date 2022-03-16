a21daacf0d249ee5ef119e32748f188da81d804a
xiaoasuna <asuna1542237464@163.com>
2022-03-16 15:09:22 +0800

    事件分析处置静态页面完成，等待与接口联调

---
+++ src/components/Echarts/funnel/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-col :span="12">
+    <tip>{{ tipname }}</tip>
+    <div id="bg" style="position: relative; left:200px">
+      <div id="text" style="position: absolute; top: 205px; left: -90px;color:#158CC6;font-weight: bold;font-size: 12px; font-family:Microsoft YaHei;">操作异常事件</div>
+      <div id="text" style="position: absolute; top: 205px; left: 10px;color:#402165;font-weight: bold;font-size: 14px; font-family:Microsoft YaHei;">90000</div>
+      <div id="text" style="position: absolute; top: 245px; left: -90px;color:#158CC6;font-weight: bold;font-size: 12px; font-family:Microsoft YaHei;">数据异常事件</div>
+      <div id="text" style="position: absolute; top: 245px; left: 10px;color:#402165;font-weight: bold;font-size: 14px; font-family:Microsoft YaHei;">90000</div>
+      <img :src="showPhoto" style="position: absolute;top:200px;left:-100px" alt="HIGH">
+      <img :src="showPhoto" style="position: absolute;top:240px;left:-100px" alt="HIGH">
+      <div id="text" style="position: absolute; top: 15px; left: 161px;color:#FFFFFF;font-size: 16px; font-family:Microsoft YaHei;">日志接入量</div>
+      <div id="text" style="position: absolute; top: 30px; left: 145px;color:#FFFFFF;font-size: 28px; font-family:Microsoft YaHei;">718.79亿</div>
+      <div id="text" style="position: absolute; top: 90px; left: 162px;color:#FFFFFF;font-size: 16px; font-family:Microsoft YaHei;">安全告警</div>
+      <div id="text" style="position: absolute; top: 105px; left: 157px;color:#FFFFFF;font-size: 28px; font-family:Microsoft YaHei;">5.03亿</div>
+      <div id="text" style="position: absolute; top: 165px; left: 162px;color:#FFFFFF;font-size: 16px; font-family:Microsoft YaHei;">有效告警</div>
+      <div id="text" style="position: absolute; top: 180px; left: 140px;color:#FFFFFF;font-size: 28px; font-family:Microsoft YaHei;">2415134</div>
+      <div id="text" style="position: absolute; top: 240px; left: 162px;color:#FFFFFF;font-size: 16px; font-family:Microsoft YaHei;">安全事件</div>
+      <div id="text" style="position: absolute; top: 255px; left: 140px;color:#FFFFFF;font-size: 28px; font-family:Microsoft YaHei;">2415134</div>
+      <img :src="showPhoto2" alt="HIGH">
+      <img :src="showPhoto" style="position: absolute;top:200px;left:320px" alt="HIGH">
+      <img :src="showPhoto" style="position: absolute;top:240px;left:320px" alt="HIGH">
+      <div id="text" style="position: absolute; top: 205px; left: 325px;color:#158CC6;font-weight: bold;font-size: 12px; font-family:Microsoft YaHei;">恶意软件事件</div>
+      <div id="text" style="position: absolute; top: 205px; left: 425px;color:#402165;font-weight: bold;font-size: 14px; font-family:Microsoft YaHei;">50000</div>
+      <div id="text" style="position: absolute; top: 245px; left: 325px;color:#158CC6;font-weight: bold;font-size: 12px; font-family:Microsoft YaHei;">其他事件</div>
+      <div id="text" style="position: absolute; top: 245px; left: 425px;color:#402165;font-weight: bold;font-size: 14px; font-family:Microsoft YaHei;">115134</div>
+    </div>
+  </el-col>
+</template>
+<script>
+import approval_url from '@/icons/echarts/textBK_s_B.png'
+import img_url from '@/icons/echarts/loudouBK.png'
+import tip from '@/components/EchartsTip'
+export default {
+  components: { tip },
+  props: {
+    tipname: { // tip内容
+      default: '日志异常分析',
+      type: String
+    },
+    address: { // 厂家内容
+      default: null,
+      type: Number
+    }
+  },
+  data() {
+    return {
+      showPhoto: approval_url,
+      showPhoto2: img_url,
+      policitalStatus: ['1']
+    }
+  },
+  computed: {},
+  watch: {
+    address: {
+      handler(val, oldVal) {
+        if (val !== oldVal) {
+          this.drawPolicitalStatus()
+        }
+      },
+      deep: true
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/navigation/eventAnalysis.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="16">
+      <el-form
+        ref="elForm"
+        :model="formData"
+        size="mini"
+        label-width="100px"
+        label-position="left"
+      >
+        <el-col :span="5">
+          <el-form-item label="请选择时间：">
+            <el-select v-model="formData.date" clearable :style="{width: '100%'}">
+              <el-option
+                v-for="(item, index) in dateOptions"
+                :key="index"
+                :label="item.label"
+                :value="item.value"
+                :disabled="item.disabled"
+              />
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-form>
+    </el-row>
+    <funnel />
+    <tree :address="address" />
+    <treecopy :address="address" />
+    <el-col :span="12">
+      <tip> 事件安全通报 </tip>
+      <el-row>
+        <el-col :span="6">
+          <div class="tip1 flex j-s a-c">
+            <span class="text">通报率</span>
+            <br>
+            <span class="textNumber">69.3%</span>
+
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="tip2 flex j-s a-c">
+            <span class="text">事件总数</span>
+            <br>
+            <span class="textNumber">3200</span>
+
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="tip3 flex j-s a-c">
+            <span class="text">通报数</span>
+            <br>
+            <span class="textNumber">2217.6</span>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="tip4 flex j-s a-c">
+            <span class="text">通报类型TOP1</span>
+            <br>
+            <span class="textNumber">僵木蠕事件</span>
+
+          </div>
+        </el-col>
+      </el-row>
+      <el-table :data="groupList">
+        <el-table-column label="通报编号" align="center" prop="userId" min-width="15%" />
+        <el-table-column label="事件名称" align="center" prop="groupOrder" min-width="30%" />
+        <el-table-column label="事件类型" align="center" prop="groupName" min-width="15%" />
+        <el-table-column label="优先级" align="center" prop="groupId" min-width="10%" />
+        <el-table-column label="通报状态" align="center" prop="searchValue" min-width="15%" />
+        <el-table-column label="最近更新时间" align="center" prop="delFlag" min-width="20%" />
+      </el-table>
+    </el-col>
+  </div>
+</template>
+<script>
+import funnel from '@/components/Echarts/funnel'
+import tree from '@/components/Echarts/tree'
+import treecopy from '@/components/Echarts/treeCopy'
+import tip from '@/components/EchartsTip'
+export default {
+  components: { funnel, tree, tip, treecopy },
+  props: [],
+  data() {
+    return {
+      formData: {
+        date: '最近30天'
+      },
+      dateOptions: [{
+        'label': '最近一天',
+        'value': 1
+      }, {
+        'label': '最近7天',
+        'value': 2
+      }, {
+        'label': '最近30天',
+        'value': 3
+      }],
+      policitalStatus: ['1'],
+      address: 1,
+      groupList: [{ 'searchValue': '已完成', 'remark': '已完成', 'groupId': '极低', 'userId': '1', 'groupName': '僵木蠕', 'groupOrder': '僵木蠕通报', 'delFlag': '2022-01-29 10:10:00' },
+        { 'searchValue': '已完成', 'remark': '已完成', 'groupId': '极低', 'userId': '2', 'groupName': '僵木蠕', 'groupOrder': '僵木蠕通报', 'delFlag': '2022-01-29 10:10:00' },
+        { 'searchValue': '已完成', 'remark': '已完成', 'groupId': '极低', 'userId': '3', 'groupName': '僵木蠕', 'groupOrder': '僵木蠕通报', 'delFlag': '2022-01-29 10:10:00' },
+        { 'searchValue': '已完成', 'remark': '已完成', 'groupId': '极低', 'userId': '4', 'groupName': '僵木蠕', 'groupOrder': '僵木蠕通报', 'delFlag': '2022-01-29 10:10:00' },
+        { 'searchValue': '已完成', 'remark': '已完成', 'groupId': '极低', 'userId': '3', 'groupName': '僵木蠕', 'groupOrder': '僵木蠕通报', 'delFlag': '2022-01-29 10:10:00' }]
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {
+
+  },
+  mounted() {
+  },
+  methods: {
+
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+.tip1 {
+  border-left: 4px solid rgba(24, 144, 255, 1);
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+.tip2 {
+  border-left: 4px solid rgba(247, 48, 48, 1);
+  font-weight: bold;
+   margin-bottom: 5px;
+}
+.tip3 {
+  border-left: 4px solid rgba(181, 146, 228, 1);
+  font-weight: bold;
+   margin-bottom: 5px;
+}
+.tip4 {
+  border-left: 4px solid rgba(240, 177, 68, 1);
+  font-weight: bold;
+   margin-bottom: 5px;
+}
+.text{
+  margin-left: 10px;
+  font-size: 12px;
+  color:rgba(51, 51, 51, 0.6);
+}
+.textNumber{
+  margin-left: 10px;
+  font-size: 20px;
+  color:rgba(51, 51, 51, 1);
+}
+</style>
