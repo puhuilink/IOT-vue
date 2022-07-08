753c4cf20b0a8980bda7d74c79901a7d41f62b2c
xiaoasuna <asuna1542237464@163.com>
2022-07-08 22:27:51 +0800

    资产分组添加增删改按钮，接口暂未调通

--- src/views/assetsManagement/assetGroupDetail/components/selectTree.vue
+++ src/views/assetsManagement/assetGroupDetail/components/selectTree.vue
@@ -13,7 +13,7 @@
           </el-col>
         </el-row>
       <el-row>
-        <el-col :span="12">
+        <el-col :span="18">
           <el-tree :data="selectList"
                    show-checkbox
                    node-key="assetId" 
@@ -21,14 +21,24 @@
                   @check="getCheckedKeys(true)"
                     :filter-node-method="filterNode"
                      :default-expanded-keys="[2]"
-                    ref="tree"/>
+                    ref="tree">
+                  <span class="custom-tree-node show-hide" slot-scope="{ node, data }">
+                <span>{{ node.label }}</span>
+                <span style="display:none;">
+                    <i  style="color:blue" @click="() => appendNode(node, data)" class="small-operation-btn el-icon-plus" title="增加"></i><!--增加节点-->
+                    <!-- 根节点不需要删除和修改 -->
+                    <i  style="color:blue" v-if="data.id !== 1" @click="() => removeNode(node,data)" class="small-operation-btn el-icon-delete" title="删除"></i><!--删除节点-->
+                    <i  style="color:blue"  v-if="data.id !== 1" @click="() => editNode(node,data)" class="small-operation-btn el-icon-edit" title="编辑"></i><!--编辑节点-->
+                </span>
+            </span>
+          </el-tree>
         </el-col>
       </el-row>
     </el-card>
   </div>
 </template>
 <script>
-import { selectAssetGruop} from "@/api/system/list";
+import { selectAssetGruop,getNatureTreeNode,putNatureTreeNode,delNatureTreeNode} from "@/api/system/list";
 export default {
   data () {
     return {
@@ -59,26 +69,81 @@ export default {
         )).join(',')
         this.$emit('checkItem', this.orgTreeData) // 这是选中的节点数组  
       },
+      //筛选tree
      filterNode(value, data) {
         if (!value) return true;
         return data.assetType.indexOf(value) !== -1;
       },
+      //获取tree
     getTableList(){
       selectAssetGruop().then(res => {
         this.selectList= res.data;
       })
     },
+    	// 新增树节点
+    appendNode(node, data) {
+      this.nodeForm.parentId = data.id;
+      getNatureTreeNode().then(res =>{
+        console.log(res);
+      });
+    },
+    // 删除树节点
+    removeNode(node, data) {
+      // 判断该节点是否有子节点
+      if(node.childNodes.length != 0){
+        this.$message({
+          message: '该节点下存在子节点，不允许直接删除',
+          type: 'warning'
+        });
+        return;
+      }
+      this.$confirm("是否确认删除此节点?", "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(function () {
+          return delNatureTreeNode(data.id);
+        })
+        .then(res => {
+      console.log(res);
+        })
+        .catch(function () {});
+    },
+    // 编辑树节点
+    editNode(node, data) {
+      putNatureTreeNode(data.id).then(res =>{
+  console.log(res);
+      });
+    }
+
     }
 };
 </script>
 <style lang="scss"
  scoped>
-::v-deep .label-type {
-  .el-form-item__label {
-    color: #333;
-    font-family: MicrosoftYaHei;
-    font-size: 14px;
-    font-weight: normal !important;
+ .custom-tree-node {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 14px;
+        padding-right: 8px;
+    }
+     .small-operation-btn {
+    margin: 0px 5px;
   }
+    .el-icon-plus:hover {
+  color: #1c92e0;
 }
+.el-icon-edit:hover {
+  color: #1c92e0;
+}
+.el-icon-delete:hover {
+  color: #1c92e0;
+}
+
+  .show-hide:hover :nth-child(2) {
+      display: inline-block !important;
+    }
 </style>
