5de143828bd8b0d42fe945caec2217df95b41f2e
quyanjie <quyanjie1991.com>
2022-08-01 10:17:07 +0800

    优化issues

--- src/views/system/equipmentSafetyStatisticalAnalysis/index.vue
+++ src/views/system/equipmentSafetyStatisticalAnalysis/index.vue
@@ -714,9 +714,11 @@ export default {
     height: 900px;
     padding-left:30px;
     // background-color: #ffff77;
+   
     .container {
       width: 100%;
       height: 800px;
+       border:1px solid #000;
     }
   }
   .right {
